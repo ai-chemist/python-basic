94f68b37a8234d0a3185285197dbf3bed41bc0ea
ai-chemist <utk8244@gmail.com>
2025-08-05 16:23:41 +0900

    Chapter 7

---
+++ chapter07/input.py
@@ -0,0 +1,15 @@
+# input() 메서드로 프롬프트 출력 및 반환 값 할당
+
+message = input("Enter your name : ")
+print(message)
+
+# 프롬프트 변수화
+prompt = "Hello, Please Enter Your Name : "
+name = input(prompt)
+print(name)
+
+# 프롬프트에 여러 줄 출력
+prompt_0 = "Welcome"
+prompt_0 += "\nPlease Enter Your Name : "
+ckeck = input(prompt_0)
+print(ckeck)
